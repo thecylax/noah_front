2168105b0891415bb33ed3a646cf864c00c53708
Robson Cardoso dos Santos <cardoso.rcs@gmail.com>
2024-08-09 15:41:49 -0300

    CHANGE  Atualizado icones

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import './globals.css'
-import { Inter } from 'next/font/google'
-import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Inter } from 'next/font/google'
 import { useEffect } from 'react'
-import { TopBar } from './components/TopBar'
 import { Footer } from './components/Footer'
+import { TopBar } from './components/TopBar'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +29,13 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css" />
+        <link rel="manifest" href="/manifest.json" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+
       </head>
 
       <body>
